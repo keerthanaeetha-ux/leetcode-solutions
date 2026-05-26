0611a88f8dc4ccaa578a6a3211783ab2299ed9a9
keerthanaeetha-ux <keerthanaeetha@gmail.com>
2026-05-26 21:41:44 +0530

    solved leetcode 7 reverse number

---
+++ numbers/7_reverse_num.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        
+        long reversed = 0;
+        while (x != 0) {
+            // Extract the last digit and append it to 'reversed'
+            reversed = reversed * 10 + (x % 10);
+            // Move to the next digit
+            x /= 10;
+        }
+        
+        // Check if the result is within the 32-bit signed integer range
+        if (reversed < INT_MIN || reversed > INT_MAX) {
+            return 0;
+        }
+        
+        return (int)reversed;
+        
+    }
+};
