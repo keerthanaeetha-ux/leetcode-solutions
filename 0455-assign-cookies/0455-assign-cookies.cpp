b0fd84dd57e3fb366e0fa39fd262b5df96f54782
keerthanaeetha-ux <keerthanaeetha@gmail.com>
2026-08-31 20:56:10 +0530

    Time: 16 ms (9.83%), Space: 44.9 MB (47.67%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -8,12 +8,9 @@ public:
         while(i<g.size() && j<s.size()){
             if(s[j]>=g[i]){
                 ans++;
-                j++;
                 i++;
             }
-            else{
-                j++;
-            }
+            j++;
         }
         return ans;
     }
