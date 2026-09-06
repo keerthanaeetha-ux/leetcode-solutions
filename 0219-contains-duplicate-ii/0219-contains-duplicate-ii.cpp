d19a94913d1adc05823585396317c51a5ebf26fa
keerthanaeetha-ux <keerthanaeetha@gmail.com>
2026-09-06 14:18:24 +0530

    Time: 111 ms (44.75%), Space: 134.3 MB (23.96%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int>nums_map;
-        //gap between two duplicate elements should be lessthan or equal to k
-        //that means the duplicate must be present in the next k elements of the current element 
+        unordered_set<int>window;
         for(int i=0;i<nums.size();i++){
-            if(nums_map.count(nums[i])){
-                if(i-nums_map[nums[i]]<=k)return true;
-            }
-             nums_map[nums[i]]=i;
-
+            if(i>k)window.erase(nums[i-k-1]);
+            if(window.count(nums[i]))return true;
+            window.insert(nums[i]);
         }
         return false;
         
